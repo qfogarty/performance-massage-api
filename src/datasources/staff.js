dbb52d87a2d372dcdc38be077a684c8e580e6fe3
Quentin Fogarty <quentin@qbfogarty.com>
2020-03-03 14:32:03 +1300

    Add more meaningful comments to staff member reducer

--- src/datasources/staff.js
+++ src/datasources/staff.js
@@ -1,33 +1,41 @@
-const {RESTDataSource} = require('apollo-datasource-rest');
+const { RESTDataSource } = require("apollo-datasource-rest");
 
 class staffAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = 'https://performace-massage.firebaseio.com';
+    this.baseURL = "https://performace-massage.firebaseio.com";
   }
 
   async getAllStaff() {
-    const response = await this.get('/staff.json');
-    return Array.isArray(response) ? response.map(staff => this.staffReducer(staff)) : [];
+    const response = await this.get("/staff.json");
+    return Array.isArray(response)
+      ? response.map(staff => this.staffReducer(staff))
+      : [];
   }
 
-  async getStaffById({staffId}) {
-    const response = await this.get('/staff.json');
+  async getStaffById({ staffId }) {
+    const response = await this.get("/staff.json");
     const member = response.find(staff => staff.id == staffId);
 
     return this.staffReducer(member);
   }
 
+  /**
+   * Use the reducer to return data in a usable format
+   * e.g.
+   *
+   * return {
+   *  id: staff.id || 0,
+   *  firstName: staff.firstName,
+   *  lastName: staff.lastName
+   * };
+   *
+   * @param staff
+   * @returns {*}
+   */
   staffReducer(staff) {
     return staff;
-
-    //return {
-    //  id: staff.id || 0,
-    //  firstName: staff.firstName,
-    //  lastName: staff.lastName
-    //};
   }
-
 }
 
 module.exports = staffAPI;
